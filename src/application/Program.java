c1243b6c374265694e9be340ec44d9d6503cd8b2
EvandroChucky <evandrochucky@gmail.com>
2020-08-02 14:55:29 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,6 +14,8 @@ public class Program {
 //		Seller seller = new Seller(21, "Antonio" , "antonio@gmail.com", new Date(), 12354.56, obj);
 //		System.out.println(seller);
 		
+		System.out.println("==== Test 01: Seller findById ===");
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = sellerDao.findById(3);
